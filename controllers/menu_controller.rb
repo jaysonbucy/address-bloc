f72ec3b7ca58bc95763973bc82fc753fe8f6a277
Jayson H Bucy <jayson.h.b@gmail.com>
2016-12-29 12:38:16 -0600

    assignment 6 menus

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -7,13 +7,16 @@ class MenuController
     @address_book = AddressBook.new
   end
 
+  InputError = "Sorry, that's an invalid input."
+
   def main_menu
     puts "Main Menu - #{address_book.entries.count} entries"
     puts "1 - View all entries"
-    puts "2 - Create an entry"
-    puts "3 - Search for an entry"
-    puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "2 - View entry number"
+    puts "3 - Create an entry"
+    puts "4 - Search for an entry"
+    puts "5 - Import entries from a CSV"
+    puts "6 - Exit"
     print "Enter your selection: "
 
     selection = gets.to_i
@@ -25,22 +28,26 @@ class MenuController
       main_menu
     when 2
       system "clear"
-      create_entry
+      view_single_entry
       main_menu
     when 3
       system "clear"
-      search_entries
+      create_entry
       main_menu
     when 4
       system "clear"
-      read_csv
+      search_entries
       main_menu
     when 5
+      system "clear"
+      read_csv
+      main_menu
+    when 6
       puts "Good Bye!"
       exit(0)
     else
       system "clear"
-      puts "Sorry, that's an invalid input."
+      puts InputError
       main_menu
     end
   end
@@ -53,6 +60,23 @@ class MenuController
     end
     system "clear"
     puts "End of entries"
+  end
+
+  def view_single_entry
+    system "clear"
+    puts "Which entry would you like to view?"
+    selection = gets.chomp.to_i
+    if !address_book.entries[selection].nil? then
+      entry = address_book.entries[selection]
+      puts entry.to_s
+      puts "Press Enter to return to the main menu"
+      gets.chomp
+      system "clear"
+    elsif selection > address_book.entries.length - 1
+      system "clear"
+      puts InputError
+      main_menu
+    end
   end
 
   def create_entry
