20f4582e49627a1a9ffa1dd4b065fd619e60f1c4
Jayson H Bucy <jayson.h.b@gmail.com>
2016-12-30 12:39:57 -0600

    checkpoint 7 import from csv

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,3 +1,4 @@
+require 'csv'
 require_relative 'entry'
 
 class AddressBook
@@ -27,4 +28,14 @@ class AddressBook
       @entries.delete(entry_to_remove)
     end
   end
+
+  def import_from_csv(file_name)
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
+  end
+
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -1,8 +1,15 @@
 require_relative '../models/address_book'
 
   RSpec.describe AddressBook do
+    let(:book) { AddressBook.new }
+
+    def check_entry(entry, expected_name, expected_number, expected_email)
+      expect(entry.name).to eq expected_name
+      expect(entry.phone_number).to eq expected_number
+      expect(entry.email).to eq expected_email
+    end
+
     describe "attributes" do
-      let(:book) {AddressBook.new}
       it "responds to entries" do
         expect(book).to respond_to(:entries)
       end
@@ -15,7 +22,6 @@ require_relative '../models/address_book'
     end
 
     describe "#add_entry" do
-      let(:book) {AddressBook.new}
       it "adds only one entry to the address book" do
         book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
         expect(book.entries.size).to eq(1)
@@ -32,7 +38,6 @@ require_relative '../models/address_book'
     end
 
     describe "#remove_entry" do
-      let(:book) {AddressBook.new}
       it "removes only one entry to the address book" do
         book.add_entry('Carrie Fisher', '010.021.1956', 'rip@maytheforcebewithyou.com')
 
@@ -47,4 +52,49 @@ require_relative '../models/address_book'
         expect(book.entries.first.name).to eq('Carrie Fisher')
       end
     end
+
+    describe "#import_from_csv" do
+      it "imports the correct number of entries" do
+        book.import_from_csv("entries.csv")
+        book_size = book.entries.size
+
+        expect(book_size).to eq(5)
+      end
+
+      it "imports the 1st entry" do
+        book.import_from_csv("entries.csv")
+        entry_one = book.entries[0]
+
+        check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+      end
+
+      it "imports the 2nd entry" do
+        book.import_from_csv("entries.csv")
+        entry_two = book.entries[1]
+
+        check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
+      end
+
+      it "imports the 3rd entry" do
+        book.import_from_csv("entries.csv")
+        entry_three = book.entries[2]
+
+        check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
+      end
+
+      it "imports the 4th entry" do
+        book.import_from_csv("entries.csv")
+        entry_four = book.entries[3]
+
+        check_entry(entry_four, "Sally", "555-555-4646", "sally@blocmail.com")
+      end
+
+      it "imports the 5th entry" do
+        book.import_from_csv("entries.csv")
+        entry_five = book.entries[4]
+
+        check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
+      end
+    end
+
   end
